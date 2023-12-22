da1f59e45925531a452b54c099576a820bfcbf66
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-12-22 14:33:15 +0700

    update storage function

--- core/src/Storage.php
+++ core/src/Storage.php
@@ -4,5 +4,13 @@ namespace Core;
 
 class Storage
 {
-    
+    static function upload($file)
+    {
+        $ext = end(explode('.',$file['name']));
+        $filename = strtotime('now').'.'.$ext;
+        $dest = 'storage/media/'.$filename;
+        copy($file['tmp_name'], $dest);
+
+        return $dest;
+    }
 }
