dfbe916dda7436419ab38c1e766dce57e63e58f0
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-12-26 20:10:14 +0700

    update storage

--- core/src/Storage.php
+++ core/src/Storage.php
@@ -7,7 +7,7 @@ class Storage
     static function upload($file)
     {
         $parent_path = Utility::parentPath();
-        $ext = end(explode('.',$file['name']));
+        $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
         $filename = strtotime('now').'.'.$ext;
         $dest = $parent_path . 'storage/media/'.$filename;
         copy($file['tmp_name'], $dest);
