fdf590c8d823de76f7f4eb267ae67226998f6db7
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-05-11 14:29:17 +0700

    update filename

--- core/src/Storage.php
+++ core/src/Storage.php
@@ -7,8 +7,9 @@ class Storage
     static function upload($file)
     {
         $parent_path = Utility::parentPath();
-        $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
-        $filename = strtotime('now').'.'.$ext;
+        // $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+        $originialName = $file['name'];
+        $filename = strtotime('now').'-'.$originialName;
         $dest = $parent_path . 'storage/media/'.$filename;
         copy($file['tmp_name'], $dest);
 
