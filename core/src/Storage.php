df1ce5d3432fafb7d57e49fbdff2deac6c5cc6f8
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-05-17 01:24:59 +0700

    add extension

--- core/src/Storage.php
+++ core/src/Storage.php
@@ -7,9 +7,9 @@ class Storage
     static function upload($file)
     {
         $parent_path = Utility::parentPath();
-        // $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+        $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
         $originialName = substr($file['name'], 0, 20);
-        $filename = strtotime('now').'-'.$originialName;
+        $filename = strtotime('now').'-'.$originialName.'.'.$ext;
         $dest = $parent_path . 'storage/media/'.$filename;
         copy($file['tmp_name'], $dest);
 
