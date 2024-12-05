a93b390aea173a927708e3a37c3505e42192bc1f
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-12-05 09:49:24 +0700

    update order number

--- core/src/Route.php
+++ core/src/Route.php
@@ -22,7 +22,7 @@ class Route {
     {
         $db    = new Database();
 
-        $query = "SELECT role_routes.route_path FROM `user_roles` JOIN roles ON roles.id = user_roles.role_id JOIN role_routes ON role_routes.role_id = user_roles.role_id WHERE user_id=$user_id";
+        $query = "SELECT role_routes.route_path FROM `user_roles` JOIN roles ON roles.id = user_roles.role_id JOIN role_routes ON role_routes.role_id = user_roles.role_id WHERE user_id=$user_id ORDER BY order_number ASC";
         $db->query = $query;
         $allowed_routes = $db->exec('all');
 
